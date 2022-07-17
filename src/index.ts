01027da4a354b77406dc140454f5d8fcea257705
depthbomb <reesepup77@hotmail.com>
2022-07-16 21:05:16 -0500

    refactor: remove built-ins

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,11 @@
-import { Cli, Builtins } from 'clipanion';
-import { TrimCommand }   from './commands/trim';
+import { Cli }         from 'clipanion';
+import { TrimCommand } from './commands/trim';
 
 const cli = new Cli({
 	binaryLabel: 'Asar Trim',
 	binaryName: 'asar-trim',
-	binaryVersion: '1.0.1',
+	binaryVersion: '1.1.0',
 });
 
 cli.register(TrimCommand);
-cli.register(Builtins.VersionCommand);
-cli.register(Builtins.HelpCommand);
-
 cli.runExit(process.argv.slice(2));
