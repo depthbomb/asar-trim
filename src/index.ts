4d9584ed69f0bb813015472713302c71f17de21f
depthbomb <depthbomb@users.noreply.github.com>
2025-09-11 16:24:48 -0500

    fix: don't bundle node_modules

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { TrimCommand } from './commands/trim';
 const cli = new Cli({
 	binaryLabel: 'Asar Trim',
 	binaryName: 'asar-trim',
-	binaryVersion: '1.2.1',
+	binaryVersion: '1.2.2',
 });
 
 cli.register(TrimCommand);
--- tsup.config.ts
+++ tsup.config.ts
@@ -10,10 +10,8 @@ export default defineConfig(() => ({
 	dts: false,
 	minify: true,
 	skipNodeModulesBundle: false,
-	sourcemap: true,
 	splitting: false,
 	keepNames: false,
-	noExternal: [/.*/],
 	tsconfig: './tsconfig.json',
 	onSuccess: 'ts-node scripts/post-build'
 }));
